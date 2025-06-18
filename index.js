dd337a05d1eae438143dc4662f70f20327474997
hendrix <amehharrison202017@gmail.com>
2025-06-18 16:08:07 +0100

    try cors ofx 04

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const swaggerSpec = swaggerJsdoc(options);
 
 dotenv.config();
 const corsOptions = {
-  origin: ["http://localhost:3000/", "https://haven-backend.onrender.com/"],
+  origin: ["http://localhost:3000", "https://haven-backend.onrender.com"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
@@ -54,13 +54,13 @@ const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.options("*", cors(corsOptions));
 app.options("*", cors(corsOptions));
