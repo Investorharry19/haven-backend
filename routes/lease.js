26fff48f21a864a63fef7ad9867044fac6e50b8f
hendrix <amehharrison202017@gmail.com>
2025-06-21 15:53:09 +0100

    correct email orute

--- routes/lease.js
+++ routes/lease.js
@@ -92,7 +92,7 @@ HavenLeaseRouter.post(
       sendLeaseFormEmail(
         email,
         email,
-        `https://localhost:3000/tenant/submit?token=${token}&propertyName=${propertyDetails.propertyName}`,
+        `https://localhost:3000/tenant/submit-lease?token=${token}&propertyName=${propertyDetails.propertyName}`,
         propertyDetails.propertyName
       );
 
