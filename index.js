9d2de39d2f99546d19e4164314f38bf432b736d8
hendrix <amehharrison202017@gmail.com>
2025-11-14 02:12:01 +0000

    normal origin

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://haven-backend.onrender.com",
-    process.env.FRONTENDURL,
+    "https://haven-frontend-topaz.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
